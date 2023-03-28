20f59cabb496baf26ba2a7396a1d52b5909c268f
JKBunny <129149185+JKBunny@users.noreply.github.com>
2023-03-28 18:32:23 +0530

    Add files via upload

---
+++ AdditionExample.java
@@ -0,0 +1,13 @@
+class AdditionExample
+{
+ public static void main(String[] args)
+{ 
+ int num1=10, num2=20, num3;
+num3=num1+num2;
+System.out.println(num1+" +"+num2+" = "+num3);
+int num4=10, num5=20,num6;
+num6=num4+num5;
+
+System.out.println(num4+" + "+num5+" ="+num6);
+}
+}
