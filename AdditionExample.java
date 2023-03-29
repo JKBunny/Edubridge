3a08496116143cc65bf7ac2cba1c56e8e5232457
PRASANNA <129149185+PrasannaPoloju@users.noreply.github.com>
2023-03-29 08:05:35 +0530

    Add files via upload

--- AdditionExample.java
+++ AdditionExample.java
@@ -2,12 +2,15 @@ class AdditionExample
 {
  public static void main(String[] args)
 { 
+	 //declare and initializing variables
  int num1=10, num2=20, num3;
 num3=num1+num2;
+//printing statement
 System.out.println(num1+" +"+num2+" = "+num3);
+//declare and initializing variables
 int num4=10, num5=20,num6;
 num6=num4+num5;
-
+//printing statement
 System.out.println(num4+" + "+num5+" ="+num6);
 }
 }
