80893f2d7ee0b5bd83e1d22deed1989edb47a39d
PRASANNA <129149185+PrasannaPoloju@users.noreply.github.com>
2023-03-29 08:07:40 +0530

    Add files via upload

---
+++ FamilyDetails.java
@@ -0,0 +1,16 @@
+class FamilyDetails
+{
+   public static void main (String [] args)
+   {
+      String father="Srinivas" ,mother="Bhagyalaxmi", city="Siddipet", state="Telangana", country="India";
+      int age=21,salary=25000;
+      long phone=1234567890L;
+      
+      System.out.println("Father Name : "+father);
+      System.out.println("Mother Name : "+mother);
+      System.out.println("Age : "+age); 
+      System.out.println("Salary : "+salary);
+      System.out.println("Phone Number : "+phone);
+      System.out.println("Address : "+city+","+state+","+country+".");
+   }
+}
---
+++ Friends.java
@@ -0,0 +1,20 @@
+class Friends 
+{
+	private static void displayNames(){
+	 System.out.println("Tommy");
+	 System.out.println("Jerry");
+	 System.out.println("Ben10");
+	 System.out.println("mr.Bean");
+	}
+	private static void displayBestie()
+	{
+         System.out.println("shinChan");
+	}
+	public static void main(String[] args) 
+	{
+		System.out.println("my friends are");
+		displayNames();
+		System.out.println("my best friend Name is:");
+		displayBestie();
+	}
+}
