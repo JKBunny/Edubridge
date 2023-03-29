080c2256ad7b7defa4176243c863ced4545d54e4
PRASANNA <129149185+PrasannaPoloju@users.noreply.github.com>
2023-03-29 08:06:04 +0530

    Add files via upload

---
+++ DatatypeExample.java
@@ -0,0 +1,25 @@
+class DatatypeExample 
+{
+	public static void main(String[] args) 
+	{
+		
+		int batchNo=9101;
+		double salary=35000.50;
+		boolean status=false;
+		char grade='B';
+		float height=5.9f;
+		long phoneNo=3456789012L;
+		byte marks=70;
+		short age=34;
+		//printing statement
+		System.out.println("Integer Value: "+batchNo);
+		System.out.println("Double Value: "+salary);
+		System.out.println("Boolean Value: "+status);
+		System.out.println("Character Value: "+grade);
+		System.out.println("Float Value: "+height);
+		System.out.println("Long Value: "+phoneNo);
+		System.out.println("Byte Value: "+marks);
+		System.out.println("Short Value: "+age);
+		
+	}
+}
